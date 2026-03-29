a3c8fd1fd7df872448e47033c05ce41e36cadea8
Artur Klemens <m.vladenache@yahoo.co.uk>
2026-03-29 14:58:14 +0200

    isbn validator more comments

--- Challenges/isbn_10_validator.py
+++ Challenges/isbn_10_validator.py
@@ -19,21 +19,22 @@ pattern = re.compile(r"^\d{9,10}[X]{0,1}$")
 
 def is_valid_isbn10(s):
     stripped = s.replace('-', '')
-    print('stripped: ', stripped)
     if not pattern.match(stripped):
         return False
 
     sum = 0
     for index, digit in enumerate(stripped, start=1):
-        print('index, digit: ', (index, digit))
         if digit == 'X':
             sum += 10 * index
         else:
             sum += int(digit) * index
-    print('sum: ', sum)
     return False if sum % 11 else True
 
 print(is_valid_isbn10("0-306-40615-2"))
 
 # I think this has a time & space complexity of O(n), because each input is iterated over only once, so both the time
-# and space increase linearly, as the input grows.
+# and space increase linearly, as the input grows. But there are two operations performed for each n (two "if" statements)
+# in which case, it's O(2n). But this is assuming that all inputs pass the initial validation, which is false.
+# So perhaps the complexity is not linear, but I can't be sure. If I refer to my notes, I need to be concerned only with
+# the "worst case scenario", which would be "all inputs pass the initial validation". Therefore, it's likely O(n) as I
+# initially said.
